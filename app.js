9e4a51a293ac07cc577c858212593ff0585f8d4c
bur1to <daniil.dedenok@gmail.com>
2022-10-15 21:06:18 +0300

    DB connection

--- app.js
+++ app.js
@@ -1,26 +1,41 @@
+const mongoose = require("mongoose");
 const express = require("express");
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded());
 
-const users = [
-    {
-        id: 1, 
-        name: "Tom",
-        age: "18"
+mongoose.connect("mongodb://localhost:27017/testDb", {useNewUrlParser: true, useUnifiedTopology: true})
+.then(() => console.log("MongoDB succesfully connected"))
+.catch((err) => console.log(err));
+
+const userSchema = mongoose.Schema({
+    _id: mongoose.Schema.Types.ObjectId,
+    name:{
+        name: String,
+        firstName: String,
+        lastName: String
     },
-    {
-        id: 2,
-        name: "Kolya",
-        age: "90"
-    },
-    {
-        id: 3,
-        name: "Volodya",
-        age: "37"
-    }
-];
+    age: Number
+});
+
+// const users = [
+//     {
+//         id: 1, 
+//         name: "Tom",
+//         age: "18"
+//     },
+//     {
+//         id: 2,
+//         name: "Kolya",
+//         age: "90"
+//     },
+//     {
+//         id: 3,
+//         name: "Volodya",
+//         age: "37"
+//     }
+// ];
 
 app.get("/users", (req, res) => {
     res.send(users);
