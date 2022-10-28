afaccb415c1554c5a644dc3b263f61de98f76102
bur1to <daniil.dedenok@gmail.com>
2022-10-28 20:49:25 +0300

    change length of first/last names

--- validations/dataValidation.js
+++ validations/dataValidation.js
@@ -1,11 +1,11 @@
 const Joi = require('joi');
 
 exports.userValidation = (data) => {
-    const schema = Joi.object({
-        firstName: Joi.string().min(3).max(20),
-        lastName: Joi.string().min(3).max(20),
-        age: Joi.number().min(1).max(120)
-    });
+  const schema = Joi.object({
+    firstName: Joi.string().min(2).max(20),
+    lastName: Joi.string().min(2).max(20),
+    age: Joi.number().min(1).max(120)
+  });
 
-    return schema.validate(data);
-}
+  return schema.validate(data);
+};
