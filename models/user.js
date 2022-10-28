c4a3d663bfab3eb84b094b8b20df574a1a9afe8d
bur1to <daniil.dedenok@gmail.com>
2022-10-28 20:48:25 +0300

    change length of first/last names

--- models/user.js
+++ models/user.js
@@ -1,13 +1,13 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 const userSchema = mongoose.Schema({
-    firstName: {type: String, minLength: 3, maxLength: 20},
-    lastName: {type: String, minLength: 3, maxLength: 20},
-    age: {type: Number, minLength: 1, maxLength: 120}
-},{
-    collection: "user",
-    versionKey: false
+  firstName: { type: String, minLength: 2, maxLength: 20 },
+  lastName: { type: String, minLength: 2, maxLength: 20 },
+  age: { type: Number, minLength: 1, maxLength: 120 }
+}, {
+  collection: 'user',
+  versionKey: false
 });
 
-const User = mongoose.model("User", userSchema);
+const User = mongoose.model('User', userSchema);
 module.exports = User;
