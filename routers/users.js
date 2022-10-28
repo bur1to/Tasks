4bb6fac4d4c6f001ffa756b23baead75d8597dd4
bur1to <daniil.dedenok@gmail.com>
2022-10-28 20:51:59 +0300

    create router

---
+++ routers/users.js
@@ -0,0 +1,22 @@
+const express = require('express');
+
+const router = express.Router();
+
+const {
+  getUsers,
+  getUser,
+  createUser,
+  updateUser,
+  deleteUser
+} = require('../controllers/users');
+
+router.get('/', getUsers);
+router.get('/:id', getUser);
+
+router.post('/', createUser);
+
+router.put('/:id', updateUser);
+
+router.delete('/:id', deleteUser);
+
+module.exports = router;
