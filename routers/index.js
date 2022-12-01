4368f0c66db3d0ef30889841268a4c946903721b
bur1to <daniil.dedenok@gmail.com>
2022-12-01 08:40:49 +0200

    create authorization router

--- routers/index.js
+++ routers/index.js
@@ -1,7 +1,9 @@
 const users = require('./users');
 const comments = require('./comments');
+const auth = require('./authorization');
 
 module.exports = {
   users,
-  comments
+  comments,
+  auth
 };
