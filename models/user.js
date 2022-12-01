0c0206c4e80a003cb3026a576f01db0e0258d382
bur1to <daniil.dedenok@gmail.com>
2022-12-01 08:39:42 +0200

    add unique setting to email

--- models/user.js
+++ models/user.js
@@ -3,6 +3,8 @@ const mongoose = require('mongoose');
 const userSchema = mongoose.Schema({
   firstName: { type: String, minLength: 2, maxLength: 20 },
   lastName: { type: String, minLength: 2, maxLength: 20 },
+  email: { type: String, unique: true },
+  password: { type: String, minLength: 5, maxLength: 30 },
   age: { type: Number, minLength: 1, maxLength: 120 }
 }, {
   collection: 'user',
