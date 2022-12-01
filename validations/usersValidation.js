30df1f61e67024994daf8ef778ca54ea41672897
bur1to <daniil.dedenok@gmail.com>
2022-12-01 08:40:10 +0200

    nothing changed

--- validations/usersValidation.js
+++ validations/usersValidation.js
@@ -4,20 +4,24 @@ const userCreateValidation = (data) => {
   const createSchema = Joi.object({
     firstName: Joi.string().min(2).max(20).required(),
     lastName: Joi.string().min(2).max(20).required(),
+    email: Joi.string().email().required(),
+    password: Joi.string().pattern(/^[a-zA-Z0-9!@#$%^&*]{5,30}$/).required(),
     age: Joi.number().min(1).max(120).required()
   });
 
-  return createSchema.validate(data);
+  return createSchema.validateAsync(data);
 };
 
 const userUpdateValidation = (data) => {
   const updateSchema = Joi.object({
     firstName: Joi.string().min(2).max(20),
     lastName: Joi.string().min(2).max(20),
+    email: Joi.string().email(),
+    password: Joi.string().pattern(/^[a-zA-Z0-9!@#$%^&*]{5,30}$/),
     age: Joi.number().min(1).max(120)
   });
 
-  return updateSchema.validate(data);
+  return updateSchema.validateAsync(data);
 };
 
 module.exports = {
