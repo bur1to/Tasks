51e1288add7d73443714dfe0e873da85aec8a844
bur1t_o <daniil.dedenok@gmail.com>
2023-01-09 19:44:03 +0200

    updated compare hashed passwords

--- controllers/authorization.js
+++ controllers/authorization.js
@@ -1,6 +1,5 @@
 const User = require('../models/user');
 const crypto = require('crypto');
-const salt = '5d9afb187818d05848770d0c804a7f1f';
 
 const authorization = (async (req, res, next) => {
   try {
@@ -12,7 +11,8 @@ const authorization = (async (req, res, next) => {
       throw new Error('Incorrect email or password');
     }
 
-   body.password = crypto.pbkdf2Sync(body.password, salt, 1000, 64, 'sha512').toString('hex');
+    const salt = user.salt;
+    body.password = crypto.pbkdf2Sync(body.password, salt, 1000, 64, 'sha512').toString('hex');
 
     if (user.password !== body.password) {
       throw new Error('Incorrect email or password');
