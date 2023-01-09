8d16c8d76bef193d2097a12137ec0ec8b9822ef0
bur1t_o <daniil.dedenok@gmail.com>
2023-01-09 18:45:53 +0200

    changed error messages

--- controllers/authorization.js
+++ controllers/authorization.js
@@ -9,13 +9,13 @@ const authorization = (async (req, res, next) => {
     const user = await User.findOne({ email: body.email});
 
     if (!user) {
-      throw new Error('Invalid email or password');
+      throw new Error('Incorrect email or password');
     }
 
    body.password = crypto.pbkdf2Sync(body.password, salt, 1000, 64, 'sha512').toString('hex');
 
     if (user.password !== body.password) {
-      throw new Error('Invalid email or password');
+      throw new Error('Incorrect email or password');
     }
 
     const userData = {
