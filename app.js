3fe63971c412f79428e4989f574a35a4ba670a2f
bur1t_o <daniil.dedenok@gmail.com>
2023-01-14 08:09:51 +0200

    assign swaggerUi, YANL and swaggerDoc variables

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const mongoose = require('mongoose');
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs');
 const express = require('express');
 const userRouter = require('./routers/users');
 const commentRouter = require('./routers/comments');
 const authRouter = require('./routers/authorization');
+const swaggerDoc = YAML.load('./swagger/docs.yaml');
 
 const app = express();
 
@@ -16,6 +19,7 @@ mongoose.connect('mongodb://localhost:27017/testDb', { useNewUrlParser: true, us
 app.use('/users', userRouter);
 app.use('/comment', commentRouter);
 app.use('/auth', authRouter);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc));
 
 app.listen(3000, () => {
   console.log('server is start...');
