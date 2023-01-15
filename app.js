fdd7f803934fa66faa194c2e096ca6cd7a578d2c
bur1to <daniil.dedenok@gmail.com>
2023-01-15 20:41:32 +0200

    refactoring variables position

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const mongoose = require('mongoose');
-const swaggerUi = require('swagger-ui-express');
 const YAML = require('yamljs');
+const swaggerUi = require('swagger-ui-express');
 const express = require('express');
 const userRouter = require('./routers/users');
 const commentRouter = require('./routers/comments');
 const authRouter = require('./routers/authorization');
+
 const swaggerDoc = YAML.load('./swagger/docs.yaml');
 
 const app = express();
@@ -13,7 +14,7 @@ app.use(express.json());
 app.use(express.urlencoded());
 
 mongoose.connect('mongodb://localhost:27017/testDb', { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('MongoDB succesfully connected'))
+  .then(() => console.log('MongoDB succesfulfy connected'))
   .catch((err) => console.log(err));
 
 app.use('/users', userRouter);
