b8e6eccc4e392cb6ae532c02a3732196b655089d
bur1to <daniil.dedenok@gmail.com>
2023-06-29 18:17:22 +0300

    modified data and connect to DB in docker

--- app.js
+++ app.js
@@ -13,12 +13,12 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded());
 
-mongoose.connect('mongodb://localhost:27017/testDb', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect('mongodb://172.17.0.2/testDb', { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB succesfully connected'))
   .catch((err) => console.log(err));
 
 app.use('/users', userRouter);
-app.use('/comment', commentRouter);
+app.use('/comments', commentRouter);
 app.use('/auth', authRouter);
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc));
 
